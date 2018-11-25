dd952d192150a754e4deac3189d947882b4c1f60
Illia <Illia_Shpak@epam.com>
2018-11-25 11:47:21 +0000

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,17 +7,8 @@ long_description 'Installs/Configures ssm-agent'
 version '0.1.0'
 chef_version '>= 12.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-issues_url 'https://github.com/illia-sh/ssm-agent/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/illia-sh/ssm-agent.git'
+issues_url 'https://github.com/illia-sh/chef-ssm-agent/issues'
+source_url 'https://github.com/illia-sh/chef-ssm-agent.git'
 
 %w(amazon centos redhat ubuntu).each do |os|
   supports os
